68b3d7ff047844813a424c563346a6e7b817b239
Carlos <gotgoodjob@gmail.com>
2025-11-10 09:20:44 +0800

    Initial Commit

---
+++ Buttons/ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsApp.swift
+//  Buttons
+//
+//  Created by Carlos Antonio dos Santos on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
